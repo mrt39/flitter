bd48e048d71e22334273bff8f4f0467c05e5bf78
mrt39 <mertoktar39@gmail.com>
2024-10-06 13:26:21 +0300

    Fix the post displaying logic in the AllPostsDisplay component. Logic is as follows: 1- When the user renders the component, shuffle the order of the posts. 2- When the user likes a post or comments on a post, the order of the posts (that was set by the shuffle initially) should remain the same. 3- When the user posts a new post, the new post should be added and displayed as the top post, with the other posts remaining in the same order as they were before.

--- src/components/AllPostsDisplay.jsx
+++ src/components/AllPostsDisplay.jsx
@@ -1,26 +1,14 @@
 /* eslint-disable react/prop-types */
-import { useContext, useState, useEffect } from "react";
-import React from 'react';
-import { Link, useNavigate } from "react-router-dom";
-import dayjs from 'dayjs';
-import CommentModal from './CommentModal.jsx';
+import { useContext, useState, useEffect, useRef } from "react";
 import PostDisplay from './PostDisplay.jsx';
 import { UserContext, AppStatesContext } from '../App.jsx';
 import Box from '@mui/material/Box';
-import { CircularProgress, Alert, Avatar } from '@mui/material';
-import { TextField, Button, List, ListItem, ListItemText, Paper, ListItemAvatar} from '@mui/material';
-import {  Card, CardContent, Typography,  IconButton, Divider } from '@mui/material';
-import { FavoriteBorder, ChatBubbleOutline } from '@mui/icons-material';
-import FavoriteIcon from '@mui/icons-material/FavoriteBorder';
-import CommentIcon from '@mui/icons-material/CommentOutlined';
+import { CircularProgress, Alert} from '@mui/material';
+import {List, ListItem} from '@mui/material';
 
-//imports for generating the url path for routing 
-import slugify from 'slugify';
 //infinite scroll 
 import InfiniteScroll from 'react-infinite-scroll-component';
 import '../styles/AllPostsDisplay.css'
-
-
 
 const AllPostsDisplay = ({fromThisUser}) => {
 
@@ -36,91 +24,134 @@ const AllPostsDisplay = ({fromThisUser}) => {
   const [likepostID, setLikePostID] = useState("")
   const [pressedLikePost, setPressedLikePost] = useState(false)
 
-
-
   const [filteredMessages, setFilteredMessages] = useState([])
 
   // Control shuffling. 
   //Create this variable to prevent the homepage AllPostsDisplay from shuffling posts when the user likes or comments on a post.
   const [shouldNotShuffle, setShouldNotShuffle] = useState(false); 
+  const [shuffledOrder, setShuffledOrder] = useState([]);
+  const [newPostAdded, setNewPostAdded] = useState(false);
 
   
+  // Create a ref to store the previous values of the dependencies
+  const prevDeps = useRef([pressedSubmitPost, imgSubmittedNavbar, imgSubmittedHomePage, refreshPosts]);
 
-
-  //fetch for getting data of all posts
+  // fetch for getting data of all posts
   useEffect(() => {
     const getMessages = () => {
-      fetch(import.meta.env.VITE_BACKEND_URL+'/getallposts', {
-      method: 'GET',
+      fetch(import.meta.env.VITE_BACKEND_URL + '/getallposts', {
+        method: 'GET',
       })
-      .then(response => {
+        .then(response => {
           if (response.ok) {
-          return response.json(); // Parse JSON when the response is successful
+            return response.json(); // Parse JSON when the response is successful
           }
           throw new Error('Network response was not ok.');
-      })
-      .then(data => {
-          //sort data by dates, descending order
-          data.sort((post1,post2) => (post1.date < post2.date) ? 1 : ((post2.date < post1.date) ? -1 : 0))
-          setAllPosts(data)
-          setLoading(false)
-      })
-      .catch(error => {
+        })
+        .then(data => {
+          // sort data by dates, descending order
+          data.sort((a, b) => new Date(b.date) - new Date(a.date));
+          setAllPosts(data);
+          setLoading(false);
+        })
+        .catch(error => {
           setError(error.message);
           console.error('Error:', error);
-          setLoading(false)
-      });
+          setLoading(false);
+        });
     };
+  
+    // Check if the useEffect has been triggered by any of the pressedSubmitPost, imgSubmittedNavbar, imgSubmittedHomePage dependencies
+    if (
+      prevDeps.current[0] !== pressedSubmitPost ||
+      prevDeps.current[1] !== imgSubmittedNavbar ||
+      prevDeps.current[2] !== imgSubmittedHomePage
+    ) {
+      setNewPostAdded(true);
+      setShouldNotShuffle(true);
+      console.log("new post added")
+    }
+  
+    // If the useEffect has been triggered by the refreshPosts dependency (like or comment), set the shouldNotShuffle state to true
+    if (prevDeps.current[3] !== refreshPosts) {
+      setShouldNotShuffle(true);
+    }
+  
+    prevDeps.current = [pressedSubmitPost, imgSubmittedNavbar, imgSubmittedHomePage, refreshPosts];
+  
     getMessages();
-    //should shuffle by default
-    setShouldNotShuffle(false)
-    }, [pressedSubmitPost, imgSubmittedNavbar, imgSubmittedHomePage, refreshPosts]); 
+  }, [pressedSubmitPost, imgSubmittedNavbar, imgSubmittedHomePage, refreshPosts]);
+  
+  // useEffect for sorting messages (shuffle or sort by date)
+  useEffect(() => {
+    sortMessageDisplay();
+  }, [allPosts]);
 
+    
+  
 
+  async function sortMessageDisplay() {
+    let filteredMessagesinFunc = [];
+  
+  //if a new post has been added, place it at the top and retain the order of the other posts
+  if (newPostAdded) {
 
+      //add the new post's index (which is 0) to the top of the shuffledOrder array, and increment the index of all the other posts by 1 
+      //so that the new post is at the top and the other posts are in the same order
+      const order = [0, ...shuffledOrder.map(index => index + 1)];
+      setShuffledOrder(order);
 
-      
+      //get the new posts from the allposts array in the new order
+      filteredMessagesinFunc = order.map(index => allPosts[index]);
 
+      setFilteredMessages(filteredMessagesinFunc);
 
-    //useffect for sorting messages
-    useEffect(() => {
-    async function sortMessageDisplay(){
-      var filteredMessages = []
+      setNewPostAdded(false);
+    return;
+    }
       //if fromThisUser exists (rendering /profile route), only get the messages from that user, display chronologically
-      if (fromThisUser){
+      if (fromThisUser) {
         allPosts.forEach((post) => {
           // Check if the post is from this user
           if (post.from[0]._id === fromThisUser._id) {
             // Push the post into filteredMessages array
-            filteredMessages.push(post);
+            filteredMessagesinFunc.push(post);
           }
         });
-        
-        // Sort filteredMessages array by date 
-        filteredMessages.sort((a, b) => new Date(b.date) - new Date(a.date));
-      }else{ //if fromThisUser does not exist (rendering home route), get all the messages, display randomly
-        filteredMessages = allPosts
-        //randomize the posts
-        for (var i = filteredMessages.length - 1; i >= 0; i--) {
-          var j = Math.floor(Math.random() * (i + 1));
-          var temp = filteredMessages[i];
-          filteredMessages[i] = filteredMessages[j];
-          filteredMessages[j] = temp;
+  
+        //sort filteredMessages array by their dates, descending order
+        filteredMessagesinFunc.sort((a, b) => new Date(b.date) - new Date(a.date));
+      } else { // If fromThisUser does not exist (rendering home route), get all the messages, shuffle them
+        //if the user has liked or commented on a post, do not shuffle the posts (which sets the shouldNotShuffle state to true), use the previously shuffled order
+        if (shouldNotShuffle && shuffledOrder.length > 0) {
+          //get the new posts from the allposts array in the stored (under shuffledOrder) order
+          filteredMessagesinFunc = shuffledOrder.map(index => allPosts[index]);
+        } else {
+            filteredMessagesinFunc = [...allPosts];
+            // Shuffle the posts
+            console.log("shuffling the posts now")
+            const order = filteredMessagesinFunc.map((_, index) => index);
+            for (let i = filteredMessagesinFunc.length - 1; i >= 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              //store the order of the posts in the shuffle
+              [order[i], order[j]] = [order[j], order[i]];
+            }
+            setShuffledOrder(order);
+            //get the posts from the allposts in the shuffled order
+            filteredMessagesinFunc = order.map(index => allPosts[index]);
+            setShouldNotShuffle(false);
         }
       }
-      setFilteredMessages(filteredMessages)
-    }
-
-    sortMessageDisplay()
-
-  }, [allPosts]);
+    
+  
+    setFilteredMessages(filteredMessagesinFunc);
+  }
 
   
   /* ---------------------------INFINITE SCROLL FUNCTIONALITY--------------------------- */
   //state for setting the visible post count, for infinite scroll functionality 
   const [visiblePosts, setVisiblePosts] = useState(10); // Initial amount of posts to show
   const [loadingPosts, setLoadingPosts] = useState(false); // Track posts loading state
-
 
   // Function to load more posts when scrolled to the bottom, with a 1.5-second delay
   function loadMorePosts () {
@@ -132,8 +163,6 @@ const AllPostsDisplay = ({fromThisUser}) => {
       setLoadingPosts(false); // End loading
     }, 1000);
   };
-
-
 
 
 
@@ -155,8 +184,6 @@ const AllPostsDisplay = ({fromThisUser}) => {
   if (error) {
     return <Alert severity="error">{error}</Alert>;
   }
-
-
 
 
   return (
@@ -189,7 +216,6 @@ const AllPostsDisplay = ({fromThisUser}) => {
           <p>No posts available.</p>
         )}
 
-
       </InfiniteScroll>
       </List>
     </Box>
@@ -197,6 +223,7 @@ const AllPostsDisplay = ({fromThisUser}) => {
   );
 };
 
-
 export default AllPostsDisplay;
+
+
 
